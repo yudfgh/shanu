c9b99288a006fad6e1397b97e08985089edd781e
Prakhar Mishra <98946887+yudfgh@users.noreply.github.com>
2024-11-25 02:14:02 +0530

    Update HelloWorldApp.java

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,5 +1,22 @@
-public class HelloWorldApp {
+package com.example.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+    @RestController
+    public class HelloWorldController {
+        @GetMapping("/")
+        public String sayHello() {
+            return "Hello, World!";
+        }
     }
 }
